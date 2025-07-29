200ff89faf1cc61513c75c2f55386de63686cf1d
Ali El-husseiny <ali.ahmed.software.engineer@gmail.com>
2025-07-29 22:48:28 +0300

    Add another Problem
    Find First Palindromic String in the Array

---
+++ Find First Palindromic String in the Array.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public string FirstPalindrome(string[] words) {
+        for(int i = 0; i < words.Length; i++)
+        {
+            bool IsPalindromic = true; 
+            for(int j = 0 ; j < words[i].Length; j++)
+            {
+                if(!(words[i][j] == words[i][words[i].Length - 1 - j]))
+                IsPalindromic = false;
+            }
+            if(IsPalindromic)
+            return words[i];            
+        }
+        return "";
+    }
+}
