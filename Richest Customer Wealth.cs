855cec95c0b568d9467ca48b5877dfa7e8e8c4cf
Ali El-husseiny <ali.ahmed.software.engineer@gmail.com>
2025-07-31 09:12:12 +0300

    Push Richest Customer Wealth.cs

---
+++ Richest Customer Wealth.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int MaximumWealth(int[][] accounts) {
+        int GlobalMax = 0;
+		int LocalMax = 0;
+		
+		for(int i = 0; i < accounts.Length; i++)
+		{
+			for(int j = 0; j < accounts[i].Length; j++)
+			{
+				LocalMax += accounts[i][j];
+			}
+			if(i == 0 || LocalMax > GlobalMax)
+				GlobalMax = LocalMax;
+			LocalMax = 0;
+		}
+		return GlobalMax;
+    }
+}
