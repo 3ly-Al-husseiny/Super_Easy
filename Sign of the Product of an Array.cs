9bfbcf13416f126d1358c07d4936a3fbb942b944
Ali El-husseiny <ali.ahmed.software.engineer@gmail.com>
2025-08-01 02:39:41 +0300

    Push Sign of the Product of an Array

---
+++ Sign of the Product of an Array.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int ArraySign(int[] nums) {
+       int Counter = 0; 
+		for(int i = 0; i < nums.Length; i ++)
+		{
+			if(nums[i] == 0) return 0;
+			
+			else if(nums[i] < 0)
+				Counter++;
+		}
+		if(Counter % 2 == 0) return 1;
+        else return -1;
+    }
+}
